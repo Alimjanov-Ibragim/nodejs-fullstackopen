4fb73995fb051b0fe7691ca2a0d898fdbc1ab50e
Ibragim <ballismlife@gmail.com>
2021-11-08 16:11:49 +0500

    Some changes

---
+++ mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.f4nck.mongodb.net/note-app?retryWrites=true`;
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+const note = new Note({
+  content: 'HTML is Easy',
+  date: new Date(),
+  important: true
+});
+
+note.save().then(result => {
+  console.log('note saved!');
+  mongoose.connection.close();
+});
