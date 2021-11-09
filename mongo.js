2071a134749bf8d4ab08ac0f60e99fc9394de8aa
Ibragim <ballismlife@gmail.com>
2021-11-09 09:52:46 +0500

    add phonebook datas

--- mongo.js
+++ mongo.js
@@ -8,6 +8,8 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2];
+const name = process.argv[3];
+const phone = process.argv[4];
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.f4nck.mongodb.net/note-app?retryWrites=true`;
 
@@ -16,7 +18,9 @@ mongoose.connect(url);
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
-  important: Boolean
+  important: Boolean,
+  name: String,
+  phone: Number
 });
 
 const Note = mongoose.model('Note', noteSchema);
@@ -24,10 +28,20 @@ const Note = mongoose.model('Note', noteSchema);
 const note = new Note({
   content: 'HTML is Easy',
   date: new Date(),
-  important: true
+  important: true,
+  name: name,
+  phone: phone
 });
 
 note.save().then(result => {
   console.log('note saved!');
+  console.log(`added ${name} number ${phone} to phonebook`);
   mongoose.connection.close();
 });
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
