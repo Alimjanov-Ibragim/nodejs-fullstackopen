eed9e11d526d4d49fc9e352b95157f1617d04e6d
Ibragim <ballismlife@gmail.com>
2021-11-12 15:49:20 +0500

    testing the backend

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,25 +1,30 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes);
-  });
+notesRouter.get('/', async (request, response) => {
+  // Note.find({}).then(notes => {
+  //   response.json(notes);
+  // });
+
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch(error => next(error));
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
 });
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+notesRouter.post('/', async (request, response) => {
   const body = request.body;
 
   const note = new Note({
@@ -28,20 +33,8 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date()
   });
 
-  note
-    .save()
-    .then(savedNote => {
-      response.json(savedNote);
-    })
-    .catch(error => next(error));
-});
-
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch(error => next(error));
+  const savedNote = await note.save();
+  response.json(savedNote);
 });
 
 notesRouter.put('/:id', (request, response, next) => {
